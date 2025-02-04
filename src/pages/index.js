17a928fb3d73b3a8b0228ade5e4731c95424fbaa
Gustavo Luna <gustavolunalimon@gmail.com>
2025-02-04 14:41:15 -0600

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Layout from "@/components/Layout";
+
+export default function Home() {
+	return (
+		<Layout>
+			<section className="hero is-danger is-fullheight">
+				<div className="hero-body">
+					<div className="">
+						<h1>IP Address Tracker</h1>
+						<p className="subtitle">Fullheight subtitle</p>
+					</div>
+				</div>
+			</section>
+		</Layout>
+	);
+}
