2b1f195ef13874e467e6f199ba71751ed08dbce6
Gustavo Luna <gustavolunalimon@gmail.com>
2025-02-05 16:02:23 -0600

    [add] map component styles

---
+++ src/components/Map.jsx
@@ -0,0 +1,6 @@
+export default function Map() {
+	return (
+		<section className="map">
+		</section>
+	)
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import Layout from "@/components/Layout";
+import Map from "@/components/Map";
 import Tracker from "@/components/Tracker";
 
 export default function Home() {
 	return (
 		<Layout>
 			<Tracker />
+			<Map />
 		</Layout>
 	);
 }
